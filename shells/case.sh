8d0b32ea4aeb072a166bf6b613720dfaa0de3e61
impelfin <jw97.park@gmail.com>
2024-03-15 12:16:05 +0900

    Shell Script

---
+++ shells/case.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+case "$1" in
+	ko) echo "Seoul" ;;
+	us) echo "Washington" ;;
+	cn) echo "Beijing" ;;
+	jp) echo "Tokyo" ;;
+	uk) echo "London" ;;
+	*) echo "Enter the country name~!!"
+esac
+
---
+++ shells/while2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+a=1
+
+while [ $a != "0" ]; do
+	echo -n "Input:"
+	read a
+
+	if [ $a -eq 0 ]; then
+		break;
+	elif [ $a -gt 9 -o $a -lt 2 ]; then
+		echo "The number of inputs must be between 2 and 9."
+	else
+		for ((k=1;k<=9;k++)) do
+		   echo "$a * $k = `expr $a \* $k `"
+		done
+	fi
+done
+echo Exit
