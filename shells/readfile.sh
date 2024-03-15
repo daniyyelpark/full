cfe09c0ee8a1eee8af452bbcaa4bb6ffd3c8fdca
impelfin <jw97.park@gmail.com>
2024-03-15 15:20:00 +0900

    readfile

--- shells/readfile.sh
+++ shells/readfile.sh
@@ -1,5 +1,9 @@
-./readfile.sh
-Line 1 : The quick brown dog jumps over the lazy fox.
-Line 2 : This is the test, this is a only a test.
-Line 3 : Romeo, Romeo!! Wherefore art thou Romeo?
-Finishing processing the file
+#!/bin/bash
+
+count=1
+cat test | while read line
+do 
+	echo "Line $count : $line"
+	count=$[ $count + 1 ]
+done
+echo "Finishing processing the file"
