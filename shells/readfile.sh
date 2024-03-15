b20c43f561aa3d1806514a07d2d3ec70aea10426
impelfin <jw97.park@gmail.com>
2024-03-15 15:03:34 +0900

    shell2

---
+++ shells/readfile.sh
@@ -0,0 +1,5 @@
+./readfile.sh
+Line 1 : The quick brown dog jumps over the lazy fox.
+Line 2 : This is the test, this is a only a test.
+Line 3 : Romeo, Romeo!! Wherefore art thou Romeo?
+Finishing processing the file
